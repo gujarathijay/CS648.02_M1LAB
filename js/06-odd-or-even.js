15bc63ea3567f677305aeb55630d78de42020cbf
gujarathijay <jaygujarathi1016@gmail.com>
2023-01-29 21:16:19 -0800

    Changes committed

--- js/06-odd-or-even.js
+++ js/06-odd-or-even.js
@@ -4,7 +4,9 @@ var i;
 for (i = 0; i <= 15; i += 1) {
     if (i % 2 === 0) {
         window.document.write(i + " is even.<br>");
+        window.console.log(i + " is even")
     } else {
         window.document.write(i + " is odd.<br>");
+        window.console.log(i + " is odd")
     }
 }
